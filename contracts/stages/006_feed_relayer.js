2fa20b1e966d937874807ebf79a2fbde4af82115
Wighawag <wighawag@gmail.com>
2020-01-17 14:18:35 +0530

    new deployments

--- contracts/stages/006_feed_relayer.js
+++ contracts/stages/006_feed_relayer.js
@@ -3,3 +3,4 @@ module.exports = async ({namedAccounts, sendTxAndWait}) => {
     const {deployer, relayer} = namedAccounts;
     await sendTxAndWait({from: deployer, to: relayer, value: '1000000000000000000'});
 }
+module.exports.skip = async () => true;
