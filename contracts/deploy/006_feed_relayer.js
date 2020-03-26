95d027a9cac87ccb8ded28a4ac8f3be00f3fde6e
Wighawag <wighawag@gmail.com>
2020-03-26 14:00:05 +0000

    update ethers + buidler-deploy

--- contracts/deploy/006_feed_relayer.js
+++ contracts/deploy/006_feed_relayer.js
@@ -4,6 +4,7 @@ module.exports = async ({namedAccounts, deployments}) => {
     const {deployer, relayer} = namedAccounts;
 
     if (chainId === '31337') {
+        console.log('feeding relayer ' + relayer);
         await sendTxAndWait({from: deployer, to: relayer, value: '1000000000000000000'});
     }
 }
